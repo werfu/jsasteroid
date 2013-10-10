0f8f36af82504c57bc456d0e737a6b2ceaa4a09d
Maxime Carey <maxime.carey@gmail.com>
2013-10-09 21:51:50 -0400

    Rotation of the rocket now work, map edge also send you back to the other edge. The acceleration is broken though.

--- main.js
+++ main.js
@@ -1,36 +1,17 @@
-function Screen(w, h) {
-	this.width = w;
-	this.height = h;
-
-	this.drawingSurface = document.createElement('canvas');
-	this.drawingSurface.id = 'dsurface';
-	this.drawingSurface.width = w;
-	this.drawingSurface.height = h;
-	
-	document.body.appendChild(this.drawingSurface);
-
-	this.ctx = this.drawingSurface.getContext("2d");
-
-	this.reset();
-}
-
-Screen.prototype = {
-	'reset' : function() {
-		this.ctx.fillStyle="#000000";
-		this.ctx.fillRect(0, 0, this.width, this.height);
-	}
-}
-
-function Rocket()
+function Rocket(eng)
 {
 	this.canvas = document.createElement('canvas');
 	this.canvas.id = 'rocket';
-	this.canvas.width = 20;
-	this.canvas.height = 20;
+	this.canvas.width = this.width = 50;
+	this.canvas.height = this.height = 50;
 
 	this.ctx = this.canvas.getContext("2d");
 	this.ctx.lineWidth = 1;
 	this.ctx.strokeStyle = "#ffffff";
+
+	this.maxspeed = 20;
+
+	this.engine = eng;
 
 	this.reset();
 }
@@ -39,21 +20,167 @@ function Rocket()
 Rocket.prototype = {
 	'reset' : function() {
 		this.angle = 0;	
-	
-		this.ctx.clearRect(0, 0, 20, 20);
-		this.ctx.beginPath();
-		this.ctx.moveTo(5,20);
-		this.ctx.lineTo(10,0);
-		this.ctx.lineTo(15,20);
-		this.ctx.moveTo(7,17);
-		this.ctx.lineTo(13, 17);
-		this.ctx.stroke();
-	}
+		this.xspeed = 0;
+		this.yspeed = 0;
 
-	'rotate' : function() {
-		this.ctx.rotate(this.angle * Math.PI / 180);
+		// Position on the screen
+		this.x = this.engine.width / 2;
+		this.y = this.engine.height / 2;
+
+		this.ctx.save();
+		this.ctx.clearRect(0, 0, this.width, this.width);
+		
+		if(typeof(DEBUG) != 'undefined') {
+			this.ctx.fillStyle = '#ff0000';
+			this.ctx.fillRect(0,0,this.width, this.height);
+		}
+
+		this.ctx.translate((this.width / 2) - 5, (this.height / 2), - 10);
+		this.ctx.beginPath();
+		this.ctx.moveTo(0,20);
+		this.ctx.lineTo(5,0);
+		this.ctx.lineTo(10,20);
+		this.ctx.moveTo(2,17);
+		this.ctx.lineTo(7, 17);
+		this.ctx.stroke();
+		this.ctx.restore();
+	
+		this.ctx.translate(this.width / 2, this.height / 2 + 10);
+	},
+	'TurnLeft' : function() { this._left++; },
+	'TurnRight' : function() { this._right++; },
+	'Trust' : function() { this._trust = true; },
+	'Fire' : function() { this._fire = true; },
+	'Tick' : function() {
+
+		this.rotate_angle = 0;
+
+		if(this._left > this._right)
+		{
+			this.rotate_angle = -6;
+		}
+		if(this._left < this._right)
+		{
+			this.rotate_angle = 6;
+		}
+		this.angle += this.rotate_angle;
+		if(this.angle > 360) this.angle -= 360;
+		if(this.angle < 0) this.angle += 360;
+
+
+		if(this._trust)
+		{
+			this.xspeed = Math.sin(this.angle) * this.maxspeed;
+			this.yspeed = Math.cos(this.angle) * this.maxspeed;	
+		}
+
+		this.x += this.xspeed;
+		if(this.x < 0) this.x = this.engine.width;
+		if(this.x > this.engine.width) this.x = 0;
+		this.y += this.yspeed;
+		if(this.y < 0) this.y = this.engine.height;
+		if(this.y > this.engine.height) this.y = 0;
+
+		this._left = this._right = 0;
+		this._trust = this._fire = false;
 	}
 };
 
-var s = new Screen(1024,728);
+function Engine(w, h) {
+	self = this;
+	self.width = w;
+	self.height = h;
+
+	// Set drawing surface
+	self.drawingSurface = document.createElement('canvas');
+	self.drawingSurface.id = 'dsurface';
+	self.drawingSurface.width = w;
+	self.drawingSurface.height = h;
+	self.ctx = self.drawingSurface.getContext("2d");
+	self.clear(); // Clear to black
+	
+	// Set keyboard handler
+	document.body.onkeydown = self.keyhandler;
+
+	// Add the canvas to the body
+	document.body.appendChild(self.drawingSurface);
+	
+	self.rocket = new Rocket(self);
+
+	self.Tick = function() {
+		self.clear();
+		self.rocket.Tick(); // Propagate the tick
+	
+
+		self.ctx.save();
+		self.ctx.translate(self.rocket.x, self.rocket.y);
+		self.ctx.rotate(self.rocket.angle  * Math.PI / 180);
+		self.ctx.drawImage(self.rocket.canvas, self.rocket.width * -0.5, self.rocket.height * -0.5 - 10);
+		self.ctx.restore();
+		
+	};
+
+	self.tickrate = 30;
+}
+
+Engine.prototype = {
+	'clear' : function() {
+		self.ctx.fillStyle="#000000";
+		self.ctx.fillRect(0, 0, self.width, self.height);
+	},
+	'reset' : function() {
+		self.clear();
+		self.rocket.reset();
+	},
+	'pause' : function() {
+		window.clearInterval(self.intervalId);
+		self.intervalId = null;
+	},
+	'resume' : function() {
+		self.intervalId = window.setInterval(self.Tick, self.tickrate);
+	},
+	'keyhandler' : function(event) {
+		if(self.intervalId == null)
+		{
+			self.resume();
+		}
+		
+		switch(event.which)
+		{
+			case 30:
+				// Space
+				self.rocket.Fire();
+				break;
+			case 37: 
+				// Left
+				self.rocket.TurnLeft();
+				break;
+			case 38:
+				// Ùp
+				self.rocket.Trust();
+				break;
+			case 39:
+				// Right
+				self.rocket.TurnRight();
+				break;
+			case 80:
+			case 112:
+				// Pause
+				self.pause();
+				break;
+			case 'S':
+				// Sound trigger
+				self.sound = !self.sound;
+				break;
+			case 8:
+				self.reset();
+				break;
+			default:
+				return;
+		}
+	}
+}
+
+
+var e = new Engine(640,480);
 
